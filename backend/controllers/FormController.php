91f6055733b2358d2cd3a398c06d312e1ec073cf
--get-all <khorchin98@gmail.com>
2022-07-26 07:50:34 +0800

    Added CORS in controller

--- backend/controllers/FormController.php
+++ backend/controllers/FormController.php
@@ -9,4 +9,27 @@ class FormController extends ActiveController
 {
     public $modelClass = Form::class;
 
+    public function behaviors()
+    {
+        $behaviours = parent::behaviors();
+
+        $auth = $behaviours['authenticator'];
+        unset($behaviours['authenticator']);
+
+        $behaviours['authenticator'] = $auth;
+        $behaviours['authenticator']['except'] = ['options'];
+
+        return \yii\helpers\ArrayHelper::merge([
+            [
+                'class' => \yii\filters\Cors::class,
+                'cors' => [
+                    'Origin' => ['*'],
+                    'Access-Control-Allow-Origin' => ['*'],
+                    'Access-Control-Request-Method' => ['POST', 'GET', 'OPTIONS'],
+                    'Access-Control-Request-Headers' => ['*'],
+                ],
+            ],
+        ], $behaviours);
+    }
+
 }
